e35673fffb0fec8b257fc206facde32d4c14db93
amami-harhid <amami-thida100@outlook.jp>
2023-11-21 18:11:19 +0900

    test8

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
 window.onload = () => {
     new p5(sketch);
+};
+
+const W = window.innerWidth;
+const H = window.innerHeight;
+
+const sketch = function (p) {
+  p.preload = function () {
+    p.dynamicLoadScript("./js/Pico.js");
   };
-  const W = window.innerWidth;
-  const H = window.innerHeight;
-  const sketch = function (p) {
-    p.preload = function () {
-      p.dynamicLoadScript("./js/Pico.js");
-    };
   
-    p.setup = function () {
-      new p.Canvas(W, H);
-      let s = new p.PicoSprite(30, 0);
-      s.scale = 1.0;
-    };
-    p.draw = function () {
-      p.background(0);
-    };
+  p.setup = function () {
+    new p.Canvas(W, H);
+    let s = new p.PicoSprite(30, 0);
+    s.scale = 1.0;
   };
+  p.draw = function () {
+    p.background(0);
+  };
+};
