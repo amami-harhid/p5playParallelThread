cb022bd61e59c9998f494009d047b84ec537bae0
amami-harhid <amami-thida100@outlook.jp>
2023-11-21 18:45:26 +0900

    test9

--- js/Pico.js
+++ js/Pico.js
@@ -11,35 +11,46 @@ p.PicoSprite = class extends p.Sprite {
   
       // 入れ子にする場合には asyncを必ずつけること
       // 入れ子には await を必ずつけること
+      let _xDirection = 1;
       const _topMethod = C.LoopForEver(async () => {
-        await C.LoopRepeat(() => {
-          this.y += 1;
-          if (this.y > H) this.y = 0;
-        }, 10);
-        await C.LoopRepeatUntil( _ => this.x > W, () => {
-            this.x += 20;
-            if (this.x > W) this.x = 0;
+        await C.LoopRepeat(10, () => {
+          this.y += 15;
+          _hosei();
+        })();
+        await C.LoopRepeatUntil( (_)=> this.x > W || this.x < 0, () => {
+            this.x += 40 * _xDirection;
           }
         )();
-        //this.x = 0;
+        _hosei();
+        this.x = 0;
+        _xDirection = _xDirection*(-1);
         await C.LoopRepeat(10, async () => {
-          this.rotation += 5;
+          this.rotation += 15;
           await C.LoopRepeat(10, () => {
-            this.x += 2;
-            this.y += 2;
-            if (this.x > W) this.x = 0;
-            if (this.y > H) this.y = 0;
-          });
+            this.x += 10 * _xDirection;
+            this.y -= 20;
+            _hosei();
+          })();
         })();
       });
+      const _hosei = ()=>{
+        if (this.x > W) this.x = 0;
+        if (this.x < 0) this.x = W;
+        if (this.y > H) this.y = 0;
+        if (this.y < 0) this.y = H;
+      };
       this.y = H/2;
       setTimeout(_topMethod, 1000);
     }
     draw() {
       super.draw();
       this.mReg.setWaitCancel();
-      //this.x += 20;
-      //if( this.x > W) this.x = 0;
+/*
+      if (this.x > W) this.x = 0;
+      if (this.x < 0) this.x = W;
+      if (this.y > H) this.y = 0;
+      if (this.y < 0) this.y = H;
+*/
     }
   };
   
